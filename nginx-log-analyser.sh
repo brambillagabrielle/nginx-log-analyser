711261dc55f6f2da9876acd11729344a5b1fba1c
gabrielle <gabriellebrambillacc@gmail.com>
2025-11-26 10:13:30 -0300

    feat: adding scripts and README

--- nginx-log-analyser.sh
+++ nginx-log-analyser.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# Top 5 IP addresses with the most requests
 echo "Top 5 IP addresses with the most requests:"
-awk '{ print $1 }' ./nginx-access.log | sort | uniq --count | sort -r | head -n 5 | awk '{ print $2 " - " $1 " request(s)" }'
+awk '{ print $1 }' nginx-access.log | sort | uniq --count | sort -r | head -n 5 | awk '{ print $2 " - " $1 " request(s)" }'
 echo
 
-# Top 5 most requested paths
 echo "Top 5 most requested paths:"
-cat nginx-access.log | grep -o 'GET /[^ ]*' | sort | uniq -c | sort -r | head -n 5 | awk '{ print $3 " - " $1 " request(s)" }'
+grep -o 'GET /[^ ]*' nginx-access.log | sort | uniq -c | sort -r | head -n 5 | awk '{ print $3 " - " $1 " request(s)" }'
 echo
 
-# Top 5 response status codes
-echo "Top 5 response status codes"
-cat nginx-access.log | grep -o 'HTTP/1.1" [0-9][0-9][0-9]' | sort | uniq -c | sort -r | head -n 5 | awk '{ print $3 " - " $1 " request(s)" }'
+echo "Top 5 response status codes:"
+grep -o 'HTTP/[^ ]* [0-9][0-9][0-9]' nginx-access.log | sort | uniq -c | sort -r | head -n 5 | awk '{ print $3 " - " $1 " request(s)" }'
 echo
 
-# Top 5 user agents
-echo "Top 5 user agents"
-awk -F '"' '{ print $6 }' ./nginx-access.log | sort | uniq --count | sort -r | head -n 5 | awk '{ for(i = 2; i <= NF; i++) printf "%s ", $i; print "- " $1 " request(s)" }'
+echo "Top 5 user agents:"
+awk -F '"' '{ print $6 }' nginx-access.log | sort | uniq --count | sort -r | head -n 5 | awk '{ for(i = 2; i <= NF; i++) printf "%s ", $i; print "- " $1 " request(s)" }'
 echo
